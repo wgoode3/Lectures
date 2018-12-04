7f8071c8ea81cc58b665277a079b5dca333a4990
William Goode <wgoode3@gmail.com>
2018-12-04 15:55:57 -0500

    Create week1day2.py

---
+++ Python/week1day2.py
@@ -0,0 +1,28 @@
+""" 
+Have students predict the output of each of the following print statements
+"""
+
+a = [1, 2.0, "abacus"]
+
+b = {"name": "Example McExampleface", "email": "example@example.com"}
+
+c = (1, 2, 3)
+
+d = 1, 2
+
+e = {"thing1": a, "thing2": b, "thing3": (a, b, c)}
+
+# print(type(a))
+# print(type(a[1]))
+# print(type(a[2]))
+# print(type(b))
+# print(type(b["name"]))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(e["thing1"]))
+# print(type(e["thing1"][2]))
+# print(type(e["thing2"]))
+# print(type(e["thing3"]))
+# print(type(e["thing3"][3]))
+# print(type(e["thing3"][2]))
